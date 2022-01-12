24d301eef633ea9ade954d1fd4df99793f6de9b5
Logan Woolf <loganwoolf@gmail.com>
2022-01-12 21:19:17 +0000

    Create middle function
    Returns the middle value or values from an array

---
+++ middle.js
@@ -0,0 +1,45 @@
+const eqArrays = (firstArr, secondArr) => {
+  if (firstArr.length !== secondArr.length) return false;
+  for (let i in firstArr) {
+    if (firstArr[i] !== secondArr[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArr, secondArr) {
+  eqArrays(firstArr, secondArr)
+    ? console.log(`👌 Assertion Passed 👌: ${firstArr} === ${secondArr}`)
+    : console.log(`💥 Assertion Failed 💥: ${firstArr} !== ${secondArr}`);
+};
+
+// return [] for 2 or fewer items
+
+// odd number of items, return middle Math.ceil(arr.length / 2)
+
+// even number, return middle two arr.length/2 arr.length/2 + 1
+
+const middle = (arr) => {
+  let outputArr = [];
+  if (arr.length <= 2) return outputArr;
+  const position = Math.floor(arr.length / 2);
+  // for odd lengths
+  if (arr.length % 2 === 1) {
+    const value = arr.slice(position, position + 1);
+    outputArr = value;
+  }
+  // for even lengths
+  if (arr.length % 2 === 0) {
+    const value = arr.slice(position - 1, position + 1);
+    outputArr = value;
+  }
+  return outputArr;
+};
+
+assertArraysEqual(
+  middle([1,2,3,4,5,6]),
+  [3,4]
+);
+assertArraysEqual(
+  middle([1,2,3,4,5,6,7]),
+  [4]
+);
