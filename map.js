d237c5004b3cf60228ecd957921ccb72d7689f88
Logan Woolf <loganwoolf@gmail.com>
2022-01-13 22:59:52 +0000

    Create map function
    Iterates over an array and returns a callback modified entry in a new array

---
+++ map.js
@@ -0,0 +1,36 @@
+// 2 args >> an array to map over, a callback function
+const eqArrays = (firstArr, secondArr) => {
+  if (firstArr.length !== secondArr.length) return false;
+  for (let i in firstArr) {
+    if (firstArr[i] !== secondArr[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArr, secondArr) {
+  eqArrays(firstArr, secondArr)
+    ? console.log(`👌 Assertion Passed 👌: ${firstArr} === ${secondArr}`)
+    : console.log(`💥 Assertion Failed 💥: ${firstArr} !== ${secondArr}`);
+};
+
+const map = function(inputArray, callbackFn) {
+  const results = [];
+  for (let item of inputArray) {
+    results.push(callbackFn(item));
+  }
+  
+  return results;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+const nums = [1,2,3,4,5];
+
+
+const firstResults = map(words, word => word[0]);
+assertArraysEqual(firstResults, ["g","c","t","m","t"]);
+
+const secondResults = map(nums, num => num * 10);
+assertArraysEqual(secondResults, [10,20,30,40,50]);
+
+const thirdResults = map(nums, num => num + 1);
+assertArraysEqual(thirdResults, [2,3,4,5,6]);
