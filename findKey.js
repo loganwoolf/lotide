389571225a7e7e93f4ba83c635aa1aee0e980f2a
Logan Woolf <loganwoolf@gmail.com>
2022-01-14 00:55:54 +0000

    Create findKey function
    Return the first key from and object whose value matches a callback

---
+++ findKey.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  actual === expected
+    ? console.log(`👌 Assertion Passed 👌: ${actual} === ${expected}`)
+    : console.log(`💥 Assertion Failed 💥: ${actual} !== ${expected}`);
+};
+
+// takes in an object and a callback
+// returns first key
+
+const findKey = function(obj, callback) {
+  for (let key in obj) {
+    console.log(obj[key]);
+    console.log(callback(obj[key]));
+    if (callback(obj[key])) return key;
+  }
+};
+
+const firstReturn = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+assertEqual(firstReturn, 'noma');
+
+const secondReturn  = findKey(
+  {
+    Logan: 37,
+    Roux: 2,
+    Melissa: 31,
+  },
+  x => x < 35
+);
+assertEqual(secondReturn, "Roux");
