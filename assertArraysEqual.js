d44ad4c094ec22bac63a58df422ab371045ab753
Logan Woolf <loganwoolf@gmail.com>
2022-01-17 18:03:07 +0000

    Implement modules on assertArraysEqual
    Move test code to test folder

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,4 @@
-const eqArrays = (firstArr, secondArr) => {
-  if (firstArr.length !== secondArr.length) return false;
-  for (let i in firstArr) {
-    if (firstArr[i] !== secondArr[i]) return false;
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(firstArr, secondArr) {
   eqArrays(firstArr, secondArr)
@@ -12,9 +6,4 @@ const assertArraysEqual = function(firstArr, secondArr) {
     : console.log(`💥 Assertion Failed 💥: ${firstArr} !== ${secondArr}`);
 };
 
-console.log(
-  assertArraysEqual([1,2,3], [1,2,3]) // expect true
-);
-console.log(
-  assertArraysEqual([1,2,3], [1,2,4]) // expect false
-);
+module.exports = assertArraysEqual;
