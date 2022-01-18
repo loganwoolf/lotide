e6613bce4083bb5039dd8fa27c80275881c1dfb9
Logan Woolf <loganwoolf@gmail.com>
2022-01-18 05:51:03 +0000

    Remove logging test lines

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,4 +1,4 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual([1,2,3], [1,2,3]); // expect true
-assertArraysEqual([1,2,3], [1,2,4]); // expect false
+// assertArraysEqual([1,2,3], [1,2,3]); // expect true
+// assertArraysEqual([1,2,3], [1,2,4]); // expect false
